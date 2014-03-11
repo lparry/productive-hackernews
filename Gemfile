195dbe968851a892f691b9f0cdf690dfaaefc8fc
Lucas Parry <lparry@gmail.com>
2014-03-11 13:47:44 -0600

    Productive Hackernews

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source "https://rubygems.org"
 ruby "2.1.1"
 gem 'sinatra'
+gem 'httparty'
+
+group :development do
+  gem "shotgun"
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,34 @@
 require 'sinatra'
+require 'httparty'
 
 get '/' do
-  "Hello, world"
+  hackernews = HTTParty.get("https://news.ycombinator.com/").to_s
+
+  # Add a bit of js to hide comments
+  hackernews.sub!(/<\/body>/, "#{<<-HIDE_COMMENTS}</body>")
+              <script>
+                var elements = document.querySelectorAll('.votearrow');
+                Array.prototype.forEach.call(elements, function(el, i){ el.style.display = 'none'; });
+
+                elements = document.querySelectorAll('.subtext');
+                Array.prototype.forEach.call(elements, function(el, i){ el.style.display = 'none'; });
+              </script>
+                  HIDE_COMMENTS
+
+  # reference hn css and images
+  hackernews.sub!(/href="news\.css\?/, "href=\"https://news.ycombinator.com/news.css?")
+  hackernews.sub!(/"y18\.gif"/, "\"https://news.ycombinator.com/y18.gif\"")
+  hackernews.gsub!(/"s\.gif"/, "\"https://news.ycombinator.com/s.gif\"")
+
+  hackernews
+end
+
+# Other sources of lost time
+get  %r{/(newslogin|news2|newest)} do
+  "nooooooope"
+end
+
+# redirect everything else to the real site
+get '/:keyword' do
+    redirect to("https://news.ycombinator.com/#{params[:keyword]}")
 end
